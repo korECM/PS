85a7340a7aa3a0c8bc5fd8b7917c9345f5e14433
ECMs <youncookiexpire@kakao.com>
2023-05-03 21:24:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int n) {
+        String s = Integer.toString(n, 3);
+        String reversed = new StringBuilder(s).reverse().toString();
+        return Integer.parseInt(reversed, 3);
+    }
+}
