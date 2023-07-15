d85779eca7cc9e4fcd88071cf83107ba0d633809
ECMs <youncookiexpire@kakao.com>
2023-07-16 00:37:02 +0900

    Time: 128 ms (83.44%), Space: 19 MB (12.40%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -3,15 +3,13 @@ class Solution:
         volume = 0
         stack = []
         for i, h in enumerate(height):
-            if not stack:
-                stack.append((i, h))
-                continue
             while stack and h > stack[-1][1]:
-                top = stack.pop()
+                _, top_height = stack.pop()
                 if len(stack) == 0:
                     break
-                distance = i - stack[-1][0] - 1
-                waters = min(h, stack[-1][1]) - top[1]
+                last_index, last_height = stack[-1]
+                distance = i - last_index - 1
+                waters = min(h, last_height) - top_height
                 volume += distance * waters
             stack.append((i, h))
         return volume
