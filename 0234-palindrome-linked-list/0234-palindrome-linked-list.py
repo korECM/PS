60a90d32f6de0548839aa241ebd718b9f2777fe8
ECMs <youncookiexpire@kakao.com>
2023-07-18 11:41:35 +0900

    Time: 647 ms (98.72%), Space: 33.5 MB (96.23%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,7 +1,13 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        data = []
-        while head is not None:
-            data.append(head.val)
-            head = head.next
-        return data == data[::-1]
+        rev: Optional[ListNode] = None
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val == slow.val:
+            rev, slow = rev.next, slow.next
+        return not rev
