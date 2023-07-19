db0a56c5c84d1a8b466c1e66ca225987d898d64b
ECMs <youncookiexpire@kakao.com>
2023-07-19 22:00:44 +0900

    Time: 82 ms (49.46%), Space: 16.4 MB (32.24%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,19 +3,16 @@ class Solution:
         dummy = head = ListNode()
         extra_num = 0
 
-        while l1 and l2:
-            extra_num, sum = divmod(l1.val + l2.val + extra_num, 10)
+        while l1 or l2 or extra_num:
+            sum = extra_num
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            extra_num, sum = divmod(sum, 10)
             head.next = ListNode(val=sum)
-            head, l1, l2 = head.next, l1.next, l2.next
-
-        remain_list = l1 if l1 else l2
-
-        while remain_list:
-            extra_num, sum = divmod(remain_list.val + extra_num, 10)
-            head.next = ListNode(val=sum)
-            head, remain_list = head.next, remain_list.next
-
-        if extra_num > 0:
-            head.next = ListNode(val=extra_num)
-
+            head = head.next
+            
         return dummy.next
