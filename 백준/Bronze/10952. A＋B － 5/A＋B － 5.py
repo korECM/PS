1cc6d502fdd7062e5ae738b118a9d0598b83f1d5
ECMs <youncookiexpire@kakao.com>
2023-07-22 14:44:21 +0900

    [Bronze V] Title: A+B - 5, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    print(a + b)
