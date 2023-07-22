f8975fac4cff9ef439fd0853c185b5a7791916c8
ECMs <youncookiexpire@kakao.com>
2023-07-22 14:38:29 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+a, b = map(int, input().split())
+
+if a > b:
+    print(">")
+elif a < b:
+    print("<")
+else:
+    print("==")
