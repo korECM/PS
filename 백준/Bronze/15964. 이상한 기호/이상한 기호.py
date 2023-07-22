292ea0c562ad2812704ab8374f9ea861cb7bac86
ECMs <youncookiexpire@kakao.com>
2023-07-23 00:50:09 +0900

    [Bronze V] Title: 이상한 기호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def num_input():
+    return int(input())
+
+
+def nums_input():
+    return map(int, input().split())
+
+
+a, b = nums_input()
+print((a + b) * (a - b))
