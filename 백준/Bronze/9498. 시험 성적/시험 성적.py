c49b31c81dbcd0b82ddcb78bf8fb53003e6253d9
ECMs <youncookiexpire@kakao.com>
2023-07-22 14:40:26 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+score = int(input())
+if 90 <= score <= 100:
+    print("A")
+elif 80 <= score < 90:
+    print("B")
+elif 70 <= score < 80:
+    print("C")
+elif 60 <= score < 70:
+    print("D")
+else:
+    print("F")
