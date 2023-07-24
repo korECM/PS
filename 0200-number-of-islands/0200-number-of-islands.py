aaa4399b63ae97b6d674aeab914e6983e9e63323
ECMs <youncookiexpire@kakao.com>
2023-07-24 16:53:36 +0900

    Time: 313 ms (85.94%), Space: 19 MB (66.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,19 +3,19 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(x, y):
+            if x < 0 or x >= len(grid[0]) or y < 0 or y >= len(grid) or grid[y][x] != '1':
+                return
+            grid[y][x] = '0'
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
+
         count = 0
         for y in range(len(grid)):
-            for x in range(len(grid[y])):
+            for x in range(len(grid[0])):
                 if grid[y][x] == '1':
-                    self.dfs(grid, x, y)
+                    dfs(x, y)
                     count += 1
         return count
-
-    def dfs(self, grid, x, y):
-        if y < 0 or y >= len(grid) or x < 0 or x >= len(grid[y]) or grid[y][x] != '1':
-            return
-        grid[y][x] = '0'
-        self.dfs(grid, x + 1, y)
-        self.dfs(grid, x - 1, y)
-        self.dfs(grid, x, y + 1)
-        self.dfs(grid, x, y - 1)
