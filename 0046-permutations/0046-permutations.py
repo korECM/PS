c652babde7e81976a046142c12013228266e280c
ECMs <youncookiexpire@kakao.com>
2023-07-25 23:44:03 +0900

    Time: 49 ms (88.47%), Space: 16.3 MB (92.48%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,7 +1,17 @@
-from itertools import permutations
 from typing import List
 
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return permutations(nums)
+        def dfs(visited: List[int], results: List[List[int]]):
+            if len(visited) == len(nums):
+                results.append(list(map(lambda x: nums[x], visited)))
+            for i in range(len(nums)):
+                if i in visited:
+                    continue
+                dfs(visited + [i], results)
+
+        results = []
+        for i in range(len(nums)):
+            dfs([i], results)
+        return results
