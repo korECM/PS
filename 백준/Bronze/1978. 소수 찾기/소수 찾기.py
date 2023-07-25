a00ca1d850c5bfce9184b2f97118328ac1d2ecc3
ECMs <youncookiexpire@kakao.com>
2023-07-25 13:00:31 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,30 @@
+import math
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def num_input():
+    return int(input())
+
+
+def nums_input():
+    return map(int, input().split())
+
+
+def is_prime(n):
+    if n == 1:
+        return False
+    if n % 2 == 0:
+        return n == 2
+    for i in range(3, math.ceil(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+n = num_input()
+nums = list(nums_input())
+print(len(list(filter(is_prime, nums))))
