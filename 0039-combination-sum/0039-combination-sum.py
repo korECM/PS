1be654c746dd8e8cfc43d64a1ad366659489d6e1
ECMs <youncookiexpire@kakao.com>
2023-07-26 00:41:18 +0900

    Time: 69 ms (83.00%), Space: 16.3 MB (98.00%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -5,7 +5,7 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         results = []
 
-        def dfs(elements: List[int] = [], start_index: int = 0, sum: int = 0):
+        def dfs(elements: List[int], start_index: int = 0, sum: int = 0):
             if sum == target:
                 results.append([*elements])
             if sum >= target:
@@ -13,5 +13,5 @@ class Solution:
             for i in range(start_index, len(candidates)):
                 dfs(elements + [candidates[i]], i, sum + candidates[i])
 
-        dfs()
+        dfs([])
         return results
