0221ebb8494699cded83f8beeec2ae6ab6acbd2d
ECMs <youncookiexpire@kakao.com>
2023-07-26 00:19:14 +0900

    Time: 384 ms (72.30%), Space: 18.3 MB (67.57%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,3 +1,19 @@
+from typing import List
+
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return list(combinations(range(1, n + 1), k))
+        results = []
+        prev_elements = []
+
+        def dfs(start: int):
+            if len(prev_elements) == k:
+                results.append([*prev_elements])
+                return
+            for i in range(start, n + 1):
+                prev_elements.append(i)
+                dfs(i + 1)
+                prev_elements.pop()
+
+        dfs(1)
+        return results
