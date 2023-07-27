a4eb434186340190f691b706e835967518b86bf6
ECMs <youncookiexpire@kakao.com>
2023-07-27 13:29:03 +0900

    Time: 99 ms (71.16%), Space: 16.9 MB (81.20%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,36 +1,19 @@
 from collections import defaultdict
-from typing import List, Dict
+from typing import List
 
 
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        candidates = []
-        itinerary_length = len(tickets) + 1
-        ticket_map = self.build_map(tickets)
-        visited = set()
+        route = []
+        graph = defaultdict(list)
+        for f, t in sorted(tickets, reverse=True):
+            graph[f].append(t)
 
-        def dfs(fr: str, path: List[str]):
-            if candidates:
-                return
-            to_list = ticket_map[fr]
-            if len(path) == itinerary_length:
-                candidates.append(path)
-                return
+        def dfs(f):
+            while graph[f]:
+                dfs(graph[f].pop())
+            route.append(f)
 
-            for to, index in to_list:
-                if candidates:
-                    return
-                if index not in visited:
-                    visited.add(index)
-                    dfs(to, path + [to])
-                    visited.remove(index)
+        dfs("JFK")
 
-        dfs("JFK", ["JFK"])
-
-        return candidates[0]
-
-    def build_map(self, tickets: List[List[str]]) -> Dict[str, List[tuple[str, int]]]:
-        ticket_map: defaultdict[str, List[tuple[str, int]]] = defaultdict(list)
-        for i, ticket in enumerate(sorted(tickets)):
-            ticket_map[ticket[0]].append((ticket[1], i))
-        return ticket_map
+        return route[::-1]
