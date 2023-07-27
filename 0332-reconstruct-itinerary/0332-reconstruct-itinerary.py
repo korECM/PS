83da9d91e56dbc7cd9230fe1fe0a9d27b2af6581
ECMs <youncookiexpire@kakao.com>
2023-07-27 13:22:13 +0900

    Time: 118 ms (14.30%), Space: 17.5 MB (8.35%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -4,28 +4,30 @@ from typing import List, Dict
 
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        candidates = ["Z"]
+        candidates = []
         itinerary_length = len(tickets) + 1
         ticket_map = self.build_map(tickets)
         visited = set()
 
-        def dfs(fr: str, path: str, path_length: int):
-            if candidates[0] != "Z":
+        def dfs(fr: str, path: List[str]):
+            if candidates:
                 return
             to_list = ticket_map[fr]
-            if path_length == itinerary_length:
-                candidates[0] = path
+            if len(path) == itinerary_length:
+                candidates.append(path)
+                return
 
             for to, index in to_list:
+                if candidates:
+                    return
                 if index not in visited:
                     visited.add(index)
-                    dfs(to, path + to, path_length + 1)
+                    dfs(to, path + [to])
                     visited.remove(index)
 
-        dfs("JFK", "JFK", 1)
+        dfs("JFK", ["JFK"])
 
-        chunks, chunks_size = len(candidates[0]), 3
-        return [candidates[0][i:i + chunks_size] for i in range(0, chunks, chunks_size)]
+        return candidates[0]
 
     def build_map(self, tickets: List[List[str]]) -> Dict[str, List[tuple[str, int]]]:
         ticket_map: defaultdict[str, List[tuple[str, int]]] = defaultdict(list)
