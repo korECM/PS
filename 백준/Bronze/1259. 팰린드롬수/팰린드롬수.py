f4661e7aa590e8ec7b105633566f95c00bbfc2a1
ECMs <youncookiexpire@kakao.com>
2023-07-27 15:52:31 +0900

    [Bronze I] Title: 팰린드롬수, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -13,18 +13,6 @@ def num_input():
 def nums_input():
     return map(int, input().split())
 
-
-def is_prime(n):
-    if n == 1:
-        return False
-    if n % 2 == 0:
-        return n == 2
-    for i in range(3, math.ceil(math.sqrt(n)) + 1, 2):
-        if n % i == 0:
-            return False
-    return True
-
-
 while True:
     n = input()
     if n == "0":
