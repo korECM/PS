a5986aa175d2a0951a89de05c8da7dfe5f09f5c8
ECMs <youncookiexpire@kakao.com>
2023-07-29 15:50:06 +0900

    Time: 64 ms (72.21%), Space: 21 MB (96.56%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -11,17 +11,14 @@ class TreeNode:
 
 
 class Solution:
-    flag: bool = True
-
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def dfs(root: Optional[TreeNode]):
-            if not self.flag or root is None:
+            if root is None:
                 return 0
             left_depth = dfs(root.left)
             right_depth = dfs(root.right)
-            if abs(left_depth - right_depth) > 1:
-                self.flag = False
+            if left_depth == -1 or right_depth == -1 or abs(left_depth - right_depth) > 1:
+                return -1
             return max(left_depth, right_depth) + 1
 
-        dfs(root)
-        return self.flag
+        return dfs(root) != -1
