5fe8ac6161bc38e97a04e70d95780b4a697d4ed6
ECMs <youncookiexpire@kakao.com>
2023-07-29 17:31:50 +0900

    [Bronze V] Title: A+B - 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def num_input():
+    return int(input())
+
+
+def nums_input():
+    return map(int, input().split())
+
+
+print(num_input() + num_input())
