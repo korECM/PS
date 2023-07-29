75f9df67c1264517b10e14681ed2e862bce0c617
ECMs <youncookiexpire@kakao.com>
2023-07-29 15:36:46 +0900

    Time: 102 ms (99.44%), Space: 22.4 MB (53.36%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -1,41 +1,41 @@
 from collections import deque
 from typing import Optional
 
+NULL = "#"
+
 
 class Codec:
 
     def serialize(self, root: Optional[TreeNode]) -> str:
         result = []
-        if root is None:
-            return str(result)
 
         queue = deque([root])
         while queue:
             for _ in range(len(queue)):
                 node = queue.popleft()
-                result.append(node.val if node else "null")
+                result.append(str(node.val) if node else NULL)
                 if node:
                     queue.append(node.left)
                     queue.append(node.right)
-        return str(result).replace("'", "").replace(', ', ',')
+        return ' '.join(result)
 
     def deserialize(self, data: str) -> Optional[TreeNode]:
-        if data == "[]":
+        if data == NULL:
             return None
-        elements = data[1:-1].split(',')
+        elements = data.split(' ')
 
         root = TreeNode(int(elements[0]), None, None)
         node_queue = deque([root])
         val_queue = deque(elements[1:])
         while val_queue:
             val1 = val_queue.popleft()
-            val2 = val_queue.popleft() if val_queue else "null"
+            val2 = val_queue.popleft() if val_queue else NULL
             node = node_queue.popleft()
 
-            if val1 != "null":
+            if val1 != NULL:
                 node.left = TreeNode(int(val1), None, None)
                 node_queue.append(node.left)
-            if val2 != "null":
+            if val2 != NULL:
                 node.right = TreeNode(int(val2), None, None)
                 node_queue.append(node.right)
         return root
