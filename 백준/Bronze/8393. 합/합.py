69b392e26dc5edb85b92c09c19537f0a93a76f97
ECMs <youncookiexpire@kakao.com>
2023-07-30 21:00:07 +0900

    [Bronze V] Title: 합, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def num_input():
+    return int(input())
+
+
+def nums_input():
+    return map(int, input().split())
+
+
+n = num_input()
+print(n * (n + 1) // 2)
