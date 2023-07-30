fc47ac1fb067c62a718fcdf5f6964d64d81a561a
ECMs <youncookiexpire@kakao.com>
2023-07-30 22:24:22 +0900

    Time: 30 ms (99.63%), Space: 16.2 MB (76.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,6 +1,17 @@
-import math
-
-
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return math.pow(x, n)
+        if n == 1:
+            return x
+
+        result = 1
+        is_negative = False
+        if n < 0:
+            is_negative = True
+            n = -n
+
+        while n > 0:
+            if n % 2 == 1:
+                result *= x
+            x *= x
+            n = n // 2
+        return 1 / result if is_negative else result
