ab63a743ac9a15643c935401207391442585bfaf
ECMs <youncookiexpire@kakao.com>
2023-08-01 20:06:57 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def num_input():
+    return int(input())
+
+
+def nums_input():
+    return map(int, input().split())
+
+
+n = num_input()
+for i in range(1, n + 1):
+    print("*" * i)
+for i in range(n - 1, 0, -1):
+    print("*" * i)
