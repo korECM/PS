d114dca96595c11f3f5fe27651fd308917103195
ECMs <youncookiexpire@kakao.com>
2023-08-01 10:44:13 +0900

    [Silver IV] Title: 제로, Time: 92 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -13,11 +13,6 @@ def nums_input():
     return map(int, input().split())
 
 
-data = [[0] * 15 for _ in range(15)]
-for i in range(15):
-    for j in range(1, 15):
-        data[i][j] = j if i == 0 else sum(data[i - 1][1:j + 1])
-
 k = num_input()
 data = []
 for _ in range(k):
