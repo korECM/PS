ac86de3c1e0a37b465f3068787db76cbaffd6eff
ECMs <youncookiexpire@kakao.com>
2023-08-04 16:37:53 +0900

    Time: 167 ms (77.94%), Space: 34.4 MB (23.47%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+
+class TrieNode:
+    word: bool
+    children: dict[str, object]
+
+    def __init__(self):
+        self.word = False
+        self.children = defaultdict(TrieNode)
+
+
+class Trie:
+    root: TrieNode
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            node = node.children[char]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
