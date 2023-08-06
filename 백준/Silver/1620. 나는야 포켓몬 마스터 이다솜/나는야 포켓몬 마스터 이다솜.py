81a0d68093242d96dddc921b673a522ca3009f54
ECMs <youncookiexpire@kakao.com>
2023-08-06 18:24:25 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 536 ms, Memory: 159848 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,41 @@
+import sys
+from typing import TypeVar
+
+
+def input() -> str:
+    return sys.stdin.readline().rstrip()
+
+
+def num_input() -> int:
+    return int(input())
+
+
+def nums_input():
+    return map(int, input().split())
+
+
+T = TypeVar("T")
+
+
+def init_array(height: int, width: int, init_val: T) -> list[list[T]]:
+    return [[init_val for _ in range(width)] for _ in range(height)]
+
+
+def move_generator(x: int, y: int,
+                   x_range: range = range(0, sys.maxsize), y_range: range = range(0, sys.maxsize)):
+    g_dx = [0, 1, -1, 0]
+    g_dy = [1, 0, 0, -1]
+    for g_i in range(4):
+        g_cx, g_cy = x + g_dx[g_i], y + g_dy[g_i]
+        if g_cx in x_range and g_cy in y_range:
+            yield g_cx, g_cy
+
+
+n, m = nums_input()
+data = {}
+for i in range(n):
+    name = input()
+    data[name] = i + 1
+    data[str(i + 1)] = name
+for _ in range(m):
+    print(data[input()])
