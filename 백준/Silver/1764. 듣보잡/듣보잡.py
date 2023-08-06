6cf8f3635f38bdebbec965587aa93edf094477e1
ECMs <youncookiexpire@kakao.com>
2023-08-06 18:27:18 +0900

    [Silver IV] Title: 듣보잡, Time: 312 ms, Memory: 133780 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,43 @@
+import sys
+from typing import TypeVar
+
+
+def input() -> str:
+    return sys.stdin.readline().rstrip()
+
+
+def num_input() -> int:
+    return int(input())
+
+
+def nums_input():
+    return map(int, input().split())
+
+
+T = TypeVar("T")
+
+
+def init_array(height: int, width: int, init_val: T) -> list[list[T]]:
+    return [[init_val for _ in range(width)] for _ in range(height)]
+
+
+def move_generator(x: int, y: int,
+                   x_range: range = range(0, sys.maxsize), y_range: range = range(0, sys.maxsize)):
+    g_dx = [0, 1, -1, 0]
+    g_dy = [1, 0, 0, -1]
+    for g_i in range(4):
+        g_cx, g_cy = x + g_dx[g_i], y + g_dy[g_i]
+        if g_cx in x_range and g_cy in y_range:
+            yield g_cx, g_cy
+
+
+n, m = nums_input()
+a = set()
+b = set()
+for _ in range(n):
+    a.add(input())
+for _ in range(m):
+    b.add(input())
+c = a & b
+print(len(c))
+print(*sorted(c), sep='\n')
