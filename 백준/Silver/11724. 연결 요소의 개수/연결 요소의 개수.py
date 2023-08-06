0d1dbc84e826f12b1bdaec084badf9e6d4a8d7d6
ECMs <youncookiexpire@kakao.com>
2023-08-06 18:53:10 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 592 ms, Memory: 189080 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,69 @@
+import sys
+from collections import defaultdict
+from typing import TypeVar
+
+
+def input() -> str:
+    return sys.stdin.readline().rstrip()
+
+
+def num_input() -> int:
+    return int(input())
+
+
+def nums_input():
+    return map(int, input().split())
+
+
+T = TypeVar("T")
+
+
+def init_array(height: int, width: int, init_val: T) -> list[list[T]]:
+    return [[init_val for _ in range(width)] for _ in range(height)]
+
+
+def move_generator(x: int, y: int,
+                   x_range: range = range(0, sys.maxsize), y_range: range = range(0, sys.maxsize)):
+    g_dx = [0, 1, -1, 0]
+    g_dy = [1, 0, 0, -1]
+    for g_i in range(4):
+        g_cx, g_cy = x + g_dx[g_i], y + g_dy[g_i]
+        if g_cx in x_range and g_cy in y_range:
+            yield g_cx, g_cy
+
+
+def create_graph() -> dict[T, list[T]]:
+    return defaultdict(list)
+
+
+def add_bidirectional_edge(graph: dict[T, list[T]], a: T, b: T):
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+def add_directional_edge(graph: dict[T, list[T]], f: T, t: T):
+    graph[f].append(t)
+
+
+n, m = nums_input()
+graph = create_graph()
+for _ in range(m):
+    a, b = nums_input()
+    add_bidirectional_edge(graph, a, b)
+
+visited = [False] * (n + 1)
+
+
+def dfs(node: int):
+    visited[node] = True
+    for near in graph[node]:
+        if not visited[near]:
+            dfs(near)
+
+
+count = 0
+for g in range(1, n + 1):
+    if not visited[g]:
+        dfs(g)
+        count += 1
+print(count)
