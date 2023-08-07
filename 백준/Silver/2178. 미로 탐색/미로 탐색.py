6d804ff4c5eb4fe756a19aee9307884538349cd1
ECMs <youncookiexpire@kakao.com>
2023-08-07 16:44:52 +0900

    [Silver I] Title: 미로 탐색, Time: 284 ms, Memory: 124916 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -6,11 +6,6 @@ from typing import TypeVar
 def input() -> str:
     return sys.stdin.readline().rstrip()
 
-
-def num_input() -> int:
-    return int(input())
-
-
 def nums_input():
     return map(int, input().split())
 
@@ -30,19 +25,6 @@ def move_generator(x: int, y: int,
         g_cx, g_cy = x + g_dx[g_i], y + g_dy[g_i]
         if g_cx in x_range and g_cy in y_range:
             yield g_cx, g_cy
-
-
-def create_graph() -> dict[T, list[T]]:
-    return defaultdict(list)
-
-
-def add_bidirectional_edge(graph: dict[T, list[T]], a: T, b: T):
-    graph[a].append(b)
-    graph[b].append(a)
-
-
-def add_directional_edge(graph: dict[T, list[T]], f: T, t: T):
-    graph[f].append(t)
 
 
 n, m = nums_input()
