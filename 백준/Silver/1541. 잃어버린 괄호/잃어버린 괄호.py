6cab86ca84732273522bd0abbfc882b195eadf65
ECMs <youncookiexpire@kakao.com>
2023-08-07 18:19:09 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,20 +1,10 @@
-import re
 import sys
 
 def input() -> str:
     return sys.stdin.readline().rstrip()
 
-split_input = re.split('(\\d+)', input())[1:-1]
-operands, operators = list(map(int, split_input[::2])), split_input[1::2]
-
-sum = operands[0]
-temp_sum = 0
-for op, operand in zip(operators, operands[1:]):
-    if op == "+":
-        if temp_sum:
-            temp_sum += operand
-        else:
-            sum += operand
-    else:
-        temp_sum += operand
-print(sum - temp_sum)
+split_input = input().split('-')
+result = 0
+for i, si in enumerate(split_input):
+    result += (-1 if i > 0 else 1) * sum(map(int, si.split("+")))
+print(result)
