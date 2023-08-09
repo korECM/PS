bae79558bc03dcf44c5e56a84e02d142fc85634c
ECMs <youncookiexpire@kakao.com>
2023-08-09 17:28:33 +0900

    [Silver I] Title: 경로 찾기, Time: 300 ms, Memory: 39864 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -22,30 +22,8 @@ T = TypeVar("T")
 def init_board(height: int, width: int, init_val: T) -> list[list[T]]:
     return [[init_val for _ in range(width)] for _ in range(height)]
 
-
-def print_board(board: list[list[any]]):
-    for b in board:
-        print(*b)
-
-
-def move_generator(x: int, y: int,
-                   x_range: range = range(0, sys.maxsize), y_range: range = range(0, sys.maxsize)):
-    g_dx = [0, 1, -1, 0]
-    g_dy = [1, 0, 0, -1]
-    for g_i in range(4):
-        g_cx, g_cy = x + g_dx[g_i], y + g_dy[g_i]
-        if g_cx in x_range and g_cy in y_range:
-            yield g_cx, g_cy
-
-
 def create_graph() -> dict[T, list[T]]:
     return defaultdict(list)
-
-
-def add_bidirectional_edge(graph: dict[T, list[T]], a: T, b: T):
-    graph[a].append(b)
-    graph[b].append(a)
-
 
 def add_directional_edge(graph: dict[T, list[T]], f: T, t: T):
     graph[f].append(t)
