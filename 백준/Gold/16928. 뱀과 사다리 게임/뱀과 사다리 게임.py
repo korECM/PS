d019578cd99b103d6078942462c30f9fa40e100d
ECMs <youncookiexpire@kakao.com>
2023-08-12 15:45:54 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 34144 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,54 +1,11 @@
 import sys
-from collections import defaultdict, deque
-from typing import TypeVar
-
+from collections import deque
 
 def input() -> str:
     return sys.stdin.readline().rstrip()
 
-
-def num_input() -> int:
-    return int(input())
-
-
 def nums_input():
     return map(int, input().split())
-
-
-T = TypeVar("T")
-
-
-def init_board(height: int, width: int, init_val: T) -> list[list[T]]:
-    return [[init_val for _ in range(width)] for _ in range(height)]
-
-
-def print_board(board: list[list[any]]):
-    for b in board:
-        print(*b)
-
-
-def move_generator(x: int, y: int,
-                   x_range: range = range(0, sys.maxsize), y_range: range = range(0, sys.maxsize)):
-    g_dx = [0, 1, -1, 0]
-    g_dy = [1, 0, 0, -1]
-    for g_i in range(4):
-        g_cx, g_cy = x + g_dx[g_i], y + g_dy[g_i]
-        if g_cx in x_range and g_cy in y_range:
-            yield g_cx, g_cy
-
-
-def create_graph() -> dict[T, list[T]]:
-    return defaultdict(list)
-
-
-def add_bidirectional_edge(graph: dict[T, list[T]], a: T, b: T):
-    graph[a].append(b)
-    graph[b].append(a)
-
-
-def add_directional_edge(graph: dict[T, list[T]], f: T, t: T):
-    graph[f].append(t)
-
 
 n, m = nums_input()
 data = {}
