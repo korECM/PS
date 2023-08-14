7592397f6e814251513cababe965bb67a0c35814
ECMs <youncookiexpire@kakao.com>
2023-08-14 15:42:06 +0900

    [Silver I] Title: 정수 삼각형, Time: 292 ms, Memory: 46396 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,65 @@
+import sys
+from collections import defaultdict
+from typing import TypeVar
+
+
+def input() -> str:
+    return sys.stdin.readline().rstrip()
+
+
+def num_input() -> int:
+    return int(input())
+
+
+def nums_input():
+    return map(int, input().split())
+
+
+T = TypeVar("T")
+
+
+def init_board(height: int, width: int, init_val: T) -> list[list[T]]:
+    return [[init_val for _ in range(width)] for _ in range(height)]
+
+
+def print_board(board: list[list[any]]):
+    for b in board:
+        print(*b)
+
+
+def move_generator(x: int, y: int,
+                   x_range: range = range(0, sys.maxsize), y_range: range = range(0, sys.maxsize)):
+    g_dx = [0, 1, -1, 0]
+    g_dy = [1, 0, 0, -1]
+    for g_i in range(4):
+        g_cx, g_cy = x + g_dx[g_i], y + g_dy[g_i]
+        if g_cx in x_range and g_cy in y_range:
+            yield g_cx, g_cy
+
+
+def create_graph() -> dict[T, list[T]]:
+    return defaultdict(list)
+
+
+def add_bidirectional_edge(graph: dict[T, list[T]], a: T, b: T):
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+def add_directional_edge(graph: dict[T, list[T]], f: T, t: T):
+    graph[f].append(t)
+
+
+n = num_input()
+board = init_board(n, n + 1, 0)
+dp = init_board(n, n + 1, 0)
+for i in range(n):
+    for j, e in enumerate(nums_input()):
+        board[i][j + 1] = e
+dp[0][1] = board[0][1]
+answer = dp[0][1]
+for i in range(1, n):
+    for j in range(1, i + 2):
+        dp[i][j] = board[i][j] + max(dp[i - 1][j], dp[i - 1][j - 1])
+        answer = max(answer, dp[i][j])
+print(answer)
