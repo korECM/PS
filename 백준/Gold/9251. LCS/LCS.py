53e4611ec5e0da4fa3e84c038278163db4c2b5a8
ECMs <youncookiexpire@kakao.com>
2023-08-18 20:05:50 +0900

    [Gold V] Title: LCS, Time: 684 ms, Memory: 60176 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,23 @@
+import sys
+from typing import TypeVar
+
+
+def input() -> str:
+    return sys.stdin.readline().rstrip()
+
+T = TypeVar("T")
+
+
+def init_board(height: int, width: int, init_val: T) -> list[list[T]]:
+    return [[init_val for _ in range(width)] for _ in range(height)]
+
+a, b = input(), input()
+dp = init_board(len(a) + 1, len(b) + 1, 0)
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i - 1] == b[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[len(a)][len(b)])
