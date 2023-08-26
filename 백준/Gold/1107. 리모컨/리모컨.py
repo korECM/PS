a12b197e07d22e3d3fd464c0706f5459c903f9f8
ECMs <youncookiexpire@kakao.com>
2023-08-26 22:13:33 +0900

    [Gold V] Title: 리모컨, Time: 332 ms, Memory: 118652 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,18 +1,4 @@
-import sys
-
-def input() -> str:
-    return sys.stdin.readline().rstrip()
-
-
-def num_input() -> int:
-    return int(input())
-
-
-def nums_input():
-    return map(int, input().split())
-
-n, m = num_input(), num_input()
-min_value = abs(n - 100)
+min_value = 0
 
 
 def solve(n: int, data: set[int], acc: str):
@@ -20,15 +6,25 @@ def solve(n: int, data: set[int], acc: str):
     if len(acc) > 6:
         return
     if acc:
-        min_value = min(min_value, abs(n - int(acc)) + len(acc))
+        new_val = abs(n - int(acc)) + len(acc)
+        if min_value > new_val:
+            min_value = new_val
     for datum in data:
         solve(n, data, acc + str(datum))
 
 
-if m == 10:
-    print(abs(n - 100))
-elif m == 0:
-    print(min(abs(n - 100), len(str(n))))
-else:
-    solve(n, set(range(0, 10)) - set(nums_input()), "")
-    print(min_value)
+def solve2():
+    global min_value
+    n, m = int(input()), int(input())
+    min_value = abs(n - 100)
+
+    if m == 10:
+        print(abs(n - 100))
+    elif m == 0:
+        print(min(abs(n - 100), len(str(n))))
+    else:
+        solve(n, set(range(0, 10)) - set(map(int, input().split())), "")
+        print(min_value)
+
+
+solve2()
