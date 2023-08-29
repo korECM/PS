39a0bb14679183ba8f87faae31c2ad4ed1393116
ECMs <youncookiexpire@kakao.com>
2023-08-29 22:35:54 +0900

    Time: 66 ms (9.51%), Space: 16.5 MB (66.73%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -8,7 +8,7 @@ class Solution:
             return [int(expression)]
         answer = []
         for i in range(len(expression)):
-            if expression[i] in "+-*/":
+            if expression[i] in "+-*":
                 for aa, bb in product(self.diffWaysToCompute(expression[:i]), self.diffWaysToCompute(expression[i + 1:])):
                     answer.append(eval(f'{aa}{expression[i]}{bb}'))
         return answer
