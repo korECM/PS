a4fc988bbb4976299005c38373fc7f49250c20cf
ECMs <youncookiexpire@kakao.com>
2023-08-29 16:02:01 +0900

    Time: 639 ms (5.25%), Space: 17.1 MB (34.64%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
@@ -0,0 +1,21 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        answer = [None for _ in range(len(people))]
+        people.sort(key=lambda x: (x[0], x[1]), reverse=True)
+        while people:
+            target = deque([people.pop()])
+            while people and target[0][0] == people[-1][0]:
+                target.append(people.pop())
+            index, count = 0, 0
+            while target and index < len(answer):
+                if count == target[0][1] and answer[index] is None:
+                    answer[index] = target.popleft()
+                    count += 1
+                if answer[index] is None:
+                    count += 1
+                index += 1
+        return answer
