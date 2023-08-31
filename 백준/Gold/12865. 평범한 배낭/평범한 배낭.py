9dc708edc70f8a9d84e82397255c4682ad3e8ef6
ECMs <youncookiexpire@kakao.com>
2023-08-31 15:56:09 +0900

    [Gold V] Title: 평범한 배낭, Time: 604 ms, Memory: 219120 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,51 +1,21 @@
-import sys
-from collections import defaultdict
+from sys import stdin as ssi
 
+class IO:
+    @staticmethod
+    def nums(): return map(int, ssi.readline().split())
 
-def input() -> str:
-    return sys.stdin.readline().rstrip()
-
-
-def num_input() -> int:
-    return int(input())
-
-
-def nums_input():
-    return map(int, input().split())
-
-
-N, K = nums_input()
-info = [(0, 0)] * N
-for i in range(N):
-    w, v = nums_input()
-    info[i] = (w, v)
-
-dp: dict[tuple[int, int], int] = defaultdict(int)
-
-
-def solve(i: int, remain: int):
-    if i < 0 or remain <= 0:
-        return 0
-    if (i, remain) in dp:
-        return dp[(i, remain)]
-    w, v = info[i]
-    if w > remain:
-        if (i - 1, remain) in dp:
-            dp[(i, remain)] = dp[(i - 1, remain)]
+N, K = map(int, ssi.readline().split())
+cargo = [[*map(int, ssi.readline().split())] for _ in range(N)]
+pack = []
+for i in range(N + 1):
+    pack.append([])
+    for c in range(K + 1):
+        if i == 0 or c == 0:
+            pack[i].append(0)
         else:
-            dp[(i, remain)] = solve(i - 1, remain)
-    else:
-        if (i - 1, remain) in dp:
-            if (i - 1, remain - w) in dp:
-                dp[(i, remain)] = max(dp[(i - 1, remain)], dp[(i - 1, remain - w)] + v)
+            weight, value = cargo[i - 1]
+            if weight <= c:
+                pack[i].append(max(pack[i - 1][c], value + pack[i - 1][c - weight]))
             else:
-                dp[(i, remain)] = max(dp[(i - 1, remain)], solve(i - 1, remain - w) + v)
-        else:
-            if (i - 1, remain - w) in dp:
-                dp[(i, remain)] = max(solve(i - 1, remain), dp[(i - 1, remain - w)] + v)
-            else:
-                dp[(i, remain)] = max(solve(i - 1, remain), solve(i - 1, remain - w) + v)
-    return dp[(i, remain)]
-
-
-print(solve(N - 1, K))
+                pack[i].append(pack[i - 1][c])
+print(pack[N][K])
