c8d57c96317c92fe64b250b925858ce0f67a8f79
ECMs <youncookiexpire@kakao.com>
2023-08-31 15:44:20 +0900

    Time: 35 ms (89.46%), Space: 16.3 MB (61.51%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,5 +1,12 @@
 class Solution:
+    dp: list[int] = [None for _ in range(0, 31)]
+
+    def __init__(self):
+        self.dp[0] = 0
+        self.dp[1] = 1
+
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        return self.fib(n - 1) + self.fib(n - 2)
+        if self.dp[n] is not None:
+            return self.dp[n]
+        self.dp[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.dp[n]
