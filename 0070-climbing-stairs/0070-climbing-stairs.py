89c8c3730ac3ab0fbf191bf3cde626373cdc3655
ECMs <youncookiexpire@kakao.com>
2023-09-01 14:55:16 +0900

    Time: 28 ms (98.25%), Space: 16.4 MB (27.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,9 @@
 class Solution:
+    dp: list[int] = [0 for _ in range(50)]
+
     def climbStairs(self, n: int) -> int:
-        dp = [[0, 0] for _ in range(max(3, n + 1))]
-        dp[1] = [1, 0]
-        dp[2] = [1, 1]
-        for i in range(3, n + 1):
-            # 한 칸
-            dp[i][0] = dp[i - 1][1] + sum(dp[i - 2])
-            # 두 칸
-            dp[i][1] = sum(dp[i - 2])
-        return sum(dp[n])
+        if n <= 2:
+            return n
+        if not self.dp[n]:
+            self.dp[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.dp[n]
