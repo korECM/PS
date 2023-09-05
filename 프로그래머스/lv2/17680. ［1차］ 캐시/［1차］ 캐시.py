4e49798645a68cf2c5c69a18ee4922fd62e09965
ECMs <youncookiexpire@kakao.com>
2023-09-05 20:29:50 +0900

    [level 2] Title: [1차] 캐시, Time: 0.90 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,19 +1,16 @@
-import heapq
+from collections import deque
 
 def solution(cacheSize: int, cities):
     if cacheSize == 0:
         return len(cities) * 5
     answer = 0
-    cache = {}
-    for i, city in enumerate(cities):
-        lo_city = city.lower()
-        if lo_city not in cache:
+    cache = deque(maxlen=cacheSize)
+    
+    for city in map(lambda s : s.lower(), cities):
+        if city not in cache:
             answer += 5
-            if len(cache) == cacheSize:
-                lru_key = min(cache, key=lambda x :cache[x])
-                del cache[lru_key]
-            cache[lo_city] = i
         else:
             answer += 1
-            cache[lo_city] = i
+            cache.remove(city)
+        cache.append(city)
     return answer
