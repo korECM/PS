0e73d061c33fe7c2e87a9f0bbfa02961c01c91e1
ECMs <youncookiexpire@kakao.com>
2023-09-05 19:57:01 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,18 +1,4 @@
 from typing import List
 
 def solution(n:int, arr1:List[int], arr2:List[int]):
-    answer = []
-    for a, b in zip(arr1, arr2):
-        line = []
-        rest = a | b
-        while rest:
-            if rest % 2 == 1:
-                line.append("#")
-            else:
-                line.append(" ")
-            rest //= 2
-        while len(line) < n:
-            line.append(" ")
-        answer.append("".join(reversed(line)))
-    print(answer)
-    return answer
+    return [bin(a|b)[2:].rjust(n, "0").replace("1", "#").replace("0", " ") for a,b in zip(arr1, arr2)]
