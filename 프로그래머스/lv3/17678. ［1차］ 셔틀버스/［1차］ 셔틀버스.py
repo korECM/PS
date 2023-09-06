750f794b6dcaff5afbe41744ee6aaa53b26220c2
ECMs <youncookiexpire@kakao.com>
2023-09-07 00:20:46 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 2.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -1,8 +1,3 @@
-from typing import List
-from collections import deque
-    
-# 40분
-    
 def str_to_min(s: str):
     hour, minute = map(int, s.split(":"))
     return hour * 60 + minute
@@ -18,13 +13,18 @@ def min_to_str(minute : int):
 def solution(n : int, t : int, m: int, timetable):
     timetable = sorted([str_to_min(t) for t in timetable], reverse=True)
     shuttle_time = str_to_min("09:00")
+    # 마지막 셔틀버스 제외하고 크루 제거
     for i in range(n - 1):
         for _ in range(m):
             if timetable and timetable[-1] <= shuttle_time:
                 timetable.pop()
         shuttle_time += t
+    # 마지막 셔틀버스를 탈 수 없는 크루 제거
     timetable = [i for i in timetable if i <= shuttle_time]
+    # 마지막 한 명 남을 때까지 크루 제거
     while m > 1 and timetable:
         timetable.pop()
         m -= 1
+    # 마지막 한 명보다 1분 일찍 타기
+    # 혹은 셔틀 시간 딱 맞춰서
     return min_to_str(timetable[-1] - 1 if timetable else shuttle_time)
