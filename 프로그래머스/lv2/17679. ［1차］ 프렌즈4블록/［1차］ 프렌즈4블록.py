a1ce533405b452ed38fb58869a4deb09af407c07
ECMs <youncookiexpire@kakao.com>
2023-09-07 17:07:18 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 4.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,36 +1,28 @@
-dx, dy = [1, 0, 0, -1], [0, 1, -1 , 0]
+def get_pos(x, y):
+    return (x, y), (x + 1, y), (x, y + 1), (x + 1, y + 1)
 
-def get_pos(x,y):
-    return ((x, y), (x+1,y), (x,y+1), (x +1, y+1))
 
-def check(m: int, n : int, x: int, y : int, board):
+def check(x: int, y: int, board):
     start = board[y][x]
-    if start == "":
+    if start == "팡":
         return False
-    if y == m - 1 or x == n - 1:
-        return False
-    target = get_pos(x,y)
-    return all([board[y][x] == board[ny][nx] for nx,ny in target])
-                
-        
+    return all([start == board[ny][nx] for nx, ny in get_pos(x, y)])
+
 
 def solution(m, n, board):
     board = [[c for c in b] for b in board]
-    answer = 0
     while True:
         target = set()
         for y in range(m - 1):
             for x in range(n - 1):
-                if check(m,n,x,y,board):
-                    for i in get_pos(x,y):
-                        target.add(i)
-        
+                if check(x, y, board):
+                    target.update(get_pos(x, y))
+
         if not target:
             break
-        answer += len(target)
 
-        for x, y in sorted(target, key=lambda t : (t[1], t[0])):
+        for x, y in sorted(target, key=lambda t: (t[1], t[0])):
             for ny in range(y, -1, -1):
-                board[ny][x] = board[ny-1][x] if ny >= 1 else ""
-        
-    return answer
+                board[ny][x] = board[ny - 1][x] if ny >= 1 else "팡"
+
+    return sum([b.count("팡") for b in board])
