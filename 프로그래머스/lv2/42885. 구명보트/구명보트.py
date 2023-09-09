d77003e5ab14e4c6dba5304c1c8b28b1530c50ed
ECMs <youncookiexpire@kakao.com>
2023-09-09 10:06:47 +0900

    [level 2] Title: 구명보트, Time: 10.79 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def solution(people, limit):
+    answer = 0
+    people = deque(sorted(people))
+    while len(people) > 1:
+        if people[0] + people[-1] <= limit:
+            people.pop()
+            people.popleft()
+        else:
+            people.pop()
+        answer += 1
+        
+    return answer + (1 if people else 0)
