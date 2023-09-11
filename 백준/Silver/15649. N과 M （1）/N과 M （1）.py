c6f9f2c796ad3caf0ede0df5e1211313c6809474
ECMs <youncookiexpire@kakao.com>
2023-09-11 15:50:29 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,29 +1,8 @@
+from itertools import permutations
 from sys import stdin as ssi
 from sys import stdout as sso
 
+N, M = map(int, ssi.readline().split())
 
-class IO:
-    @staticmethod
-    def nums(): return map(int, ssi.readline().split())
-
-    @staticmethod
-    def println(*args: any, sep: str = ' '): sso.write(sep.join(map(str, args)) + '\n')
-
-
-N, M = IO.nums()
-
-
-def dfs(depth, check: set[int], acc: list[int]):
-    if len(check) == M:
-        IO.println(*acc)
-        return
-    for i in range(1, N + 1):
-        if i not in check:
-            check.add(i)
-            acc.append(i)
-            dfs(depth + 1, check, acc)
-            acc.pop()
-            check.remove(i)
-
-
-dfs(0, set(), [])
+for result in permutations(range(1, N + 1), M):
+    sso.write(' '.join(map(str, result)) + '\n')
