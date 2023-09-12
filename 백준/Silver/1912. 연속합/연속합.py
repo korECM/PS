d455cf8bfb6a6b67d60a49ae01476c680e0b0ff2
ECMs <youncookiexpire@kakao.com>
2023-09-12 19:03:13 +0900

    [Silver II] Title: 연속합, Time: 128 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -5,8 +5,9 @@ N = int(ssi.readline())
 data = [*map(int, ssi.readline().split())]
 for i in range(1, N):
     data[i] += data[i - 1]
+
 min_v, answer = sys.maxsize, data[0]
-for a in data:
-    answer = max(answer, a, a - min_v)
-    min_v = min(min_v, a)
+for e in data:
+    answer = max(answer, e, e - min_v)
+    min_v = min(min_v, e)
 print(answer)
